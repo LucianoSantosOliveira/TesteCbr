c24a71cec344ead6083e95232ecf9054b3532ea0
luciano <lucianoliv2@hotmail.com>
2022-10-14 21:51:55 -0300

    MOdel e ajuste requisição

---
+++ TesteCbr/Models/Traducao.cs
@@ -0,0 +1,16 @@
+﻿namespace TesteCbr.Models
+{
+    public class TraducaoModel
+    {
+        public int Id { get; set; }
+        public string? PreferredLabel { get; set; }
+        public string? Traducao { get; set; }
+        public string? Confirmar { get; set; }
+        public string? Sugestao { get; set; }
+        public string? Responsavel { get; set; }
+        public string? Timestamp { get; set; }
+        public string? Validacao { get; set; }
+        public string? Sinonimo { get; set; }
+        public int ID_Login { get; set; }
+    }
+}
