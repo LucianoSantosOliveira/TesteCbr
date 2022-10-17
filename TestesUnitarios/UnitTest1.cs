a92cb7c678747b14fb4fde54f2fbe73e3d786414
luciano <lucianoliv2@hotmail.com>
2022-10-17 01:02:10 -0300

    Teste Unitario

---
+++ TestesUnitarios/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TesteCbr.Service;
+
+namespace TestesUnitarios
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
